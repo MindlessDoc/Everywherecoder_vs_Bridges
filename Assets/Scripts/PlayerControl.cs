7a86103e3da80da8fc284afe2ab07f2ef705675d
tkozlov <t.kozlov@nicetu.spb.ru>
2022-07-09 16:48:29 +0300

    Настроил управление игроком

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class PlayerControl : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] private Rigidbody2D _rigidbody;
+    [SerializeField] private float _speed;
+
+    private Vector2 moved;
+
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.A))
+            _rigidbody.MovePosition(Vector2.left * Time.deltaTime * _speed + _rigidbody.position);
+        if (Input.GetKey(KeyCode.D))
+            _rigidbody.MovePosition(Vector2.right * Time.deltaTime * _speed + _rigidbody.position);
+        if (Input.GetKey(KeyCode.W))
+            _rigidbody.MovePosition(Vector2.up * Time.deltaTime * _speed + _rigidbody.position);
+        if (Input.GetKey(KeyCode.S))
+            _rigidbody.MovePosition(Vector2.down * Time.deltaTime * _speed + _rigidbody.position);
     }
 }
+
